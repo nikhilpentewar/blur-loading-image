9265eacaf224a997a6e8e10ecb39d76ae4ba7196
Nikhil Pentewar <97288642+nikhilpentewar@users.noreply.github.com>
2022-05-07 10:43:17 +0530

    Update script.js

--- JS blur loading/script.js
+++ JS blur loading/script.js
@@ -1,4 +1,6 @@
+
 const loadText = document.querySelector('.loading-text')
+
 const bg = document.querySelector('.bg')
 
 let load = 0
@@ -10,13 +12,16 @@ function blurring() {
 
     if (load > 99) {
         clearInterval(int)
+        
     }
 
     loadText.innerText = `${load}%`
     loadText.style.opacity = scale(load, 0, 100, 1, 0)
     bg.style.filter = `blur(${scale(load, 0, 100, 30, 0)}px)`
+    
 }
 
 const scale = (num, in_min, in_max, out_min, out_max) => {
     return ((num - in_min) * (out_max - out_min)) / (in_max - in_min) + out_min
+    
   }
